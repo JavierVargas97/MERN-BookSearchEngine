aee206d290e168bab320f9b8ae9761cb881d6636
javiervargas <javiervg1802@gmail.com>
2022-08-23 02:05:30 -0500

    rename line 6

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
-import { ApolloServer } from 'apollo-server-express';
+const { ApolloServer } = require('apollo-server-express');
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
