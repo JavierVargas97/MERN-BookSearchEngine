766a62ac8118fc25445c6b56c991a347df2ab200
javiervargas <javiervg1802@gmail.com>
2022-08-22 22:55:04 -0500

    apollo server inserted on server.js, schemas folder add

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+import { ApolloServer } from 'apollo-server-express';
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
 
 // if we're in production, serve client/build as static assets
+
+//Apollo server 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
 db.once('open', () => {
+  server.start();
+  server.applyMiddleware({ app });
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
